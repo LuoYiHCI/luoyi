64724f5351e44c969331a0c17f20785256edc64c
LuoYiHCI <127479856+LuoYiHCI@users.noreply.github.com>
2023-03-13 20:57:47 +0800

    Create deeznuts.py

---
+++ deeznuts.py
@@ -0,0 +1,12 @@
+import random
+def nut(word):
+    print("sus")
+    for i in range(69):
+        print(word+"us is sus")
+noun = ['chicken', 'kaya toast', 'nut', 'sniper', 'shredder', 'meng meng', 'assassin', 'sussy baka', 'mogus', 'imposta', 'darrel', 'Tristiano Ronaldo', 'ding dong man', 'my foot', 'Mosieur France', 'genetically modified chimken aka Ayden', 'Yang An Yi', 'you']
+verb = ['does', 'kicks',  'punches', 'attacks', 'activates', 'slaughters', 'codes', 'susses', 'creates', 'lays', 'shreds', 'assassinates', 'roasts', 'rolls', 'rickrolls', 'swallows', 'cooks', 'suiiiis', 'dings', 'dongs', 'smells', 'whacks', 'wrecks', 'throws', 'modifies', 'rages at', 'slays', 'executes', "says, 'Greetings mortals!' to", 'throws a duster at', 'slams on the ground', 'corrupts', 'infiltrates', 'diffuses', 'vaporises', 'osmosises', 'sends to Jesus', 'asks', 'launches', 'hears', 'tortures', 'bullies', 'rebells', 'kisses', 'kisses', 'kisses', 'kisses']
+place = ['China', 'Ohio', 'Yishun', 'Oklahoma', 'Singapore', 'Hwa Chong', 'Nanyang Girls School', 'RI', 'RGS', 'cemetry', 'shopee mall', 'toilet', 'roller coaster', '1i4 classroom', 'haunted canteen', 'computer lab', 'contaminated science lab', 'clock tower', 'gym']
+time = ['midnight', 'dawn', 'night', 'national day', 'CNY', "ur mom's birthday", 'morning', 'afternoon', 'lunchtime', 'thirteen o clock']
+def newsentence(n):
+    for i in range(n):
+        print('A', random.choice(noun), random.choice(verb), 'a', random.choice(noun), 'in', random.choice(place), 'during', random.choice(time))
